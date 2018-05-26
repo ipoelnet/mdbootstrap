ecbf7d456eb0a175407354720ebde26d674b69af
ipoelnet <ipoelnet@gmail.com>
2018-05-27 01:04:16 +0700

    Update assets.js

--- assets.js
+++ assets.js
@@ -1,7 +1,7 @@
 new WOW().init();
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').focus()
-})
+});
 $(document).ready(function () {
     $('.mdb-select').material_select();
 });
